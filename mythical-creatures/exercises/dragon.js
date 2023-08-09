499fc9961ed1d07c58ee447b54bb00727a6fe353
Tori Enyart <torienyart@gmail.com>
2023-08-09 14:16:28 -0400

    Complete functions for dragon mythical creature

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,33 @@
+function createDragon(name, rider, temperment) {
+  return {
+    name: name,
+    rider: rider,
+    temperment: temperment,
+    timesEaten: 0,
+    hungry: true
+  }
+}
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon) {
+  dragon.timesEaten += 1;
+  if (dragon.timesEaten >= 3) {
+    dragon.hungry = false
+  }
+  return dragon
+}
+
+function findFireBreathers(dragons) {
+  return dragons.filter(dragon => dragon.temperment === 'aggressive')
+}
 
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers
 }
